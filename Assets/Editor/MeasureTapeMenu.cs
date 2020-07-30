2e647d73f0455d6e6ff10820a83bca6eb3c35c21
kalucky0 <kalucki23@gmail.com>
2020-07-30 15:26:04 +0200

    Initial commit

---
+++ Assets/Editor/MeasureTapeMenu.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEditor;
+
+public class MeasureTapeMenu : MonoBehaviour
+{
+    [MenuItem("GameObject/Create MeasureTape", false, 0)]
+    static void CreateMeasureTape()
+    {
+        if (Selection.activeGameObject != null)
+        {
+            if (Selection.activeGameObject.name == "MeasureTape")
+            {
+                addNewMeasureTape(Selection.activeGameObject);
+            }
+            else
+            {
+                if (GameObject.Find("MeasureTape") != null)
+                    EditorUtility.DisplayDialog("MeasureTape Warning", "Oops, You need to select a MeasureTape to add an additional copy of the tool.", "OK");
+                else
+                    createNewMeasureTape();
+            }
+        }
+        else
+        {
+            if (GameObject.Find("MeasureTape") != null)
+                addNewMeasureTape(GameObject.Find("MeasureTape"));
+            else
+                createNewMeasureTape();
+        }
+    }
+
+    static void createNewMeasureTape()
+    {
+        GameObject go = new GameObject("MeasureTape");
+        go.transform.position = Vector3.zero;
+        go.AddComponent(typeof(MeasureTape));
+    }
+
+    static void addNewMeasureTape(GameObject go)
+    {
+        go.AddComponent(typeof(MeasureTape));
+    }
+}
---
+++ Assets/MeasureTape.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class MeasureTape : MonoBehaviour
+{
+    public Color lineColor = Color.yellow;
+    public bool initialized = false;
+    public Vector3 startPoint = Vector3.zero;
+    public Vector3 endPoint = new Vector3(0, 1, 0);
+    public float distance;
+    public float gizmoSize = 0.1f;
+    public bool showCentimeters = true;
+    public bool scaleToPixels = false;
+    public int pixelsPerUnit = 128;
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = this.lineColor;
+        Gizmos.DrawCube(startPoint, new Vector3(gizmoSize, gizmoSize, gizmoSize));
+        Gizmos.DrawCube(endPoint, new Vector3(gizmoSize, gizmoSize, gizmoSize));
+        Gizmos.DrawLine(startPoint, endPoint);
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = this.lineColor;
+        Gizmos.DrawCube(startPoint, new Vector3(gizmoSize, gizmoSize, gizmoSize));
+        Gizmos.DrawCube(endPoint, new Vector3(gizmoSize, gizmoSize, gizmoSize));
+        Gizmos.DrawLine(startPoint, endPoint);
+    }
+}
+
